b1b85b8660c1020e066de5216932978e35d7e95d
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2025-10-06 11:31:50 +0200

    Added JS_Basics

---
+++ JS_Basics/main.js
@@ -0,0 +1,40 @@
+// function start() {
+//   const firstName = "Sebastian";
+//   let age = 46;
+//   const isMarried = true;
+
+//   console.log("Namn:", firstName);
+//   console.log("Ålder:", age);
+//   console.log("Civilstatus:", isMarried);
+// }
+
+let age = 46;
+
+console.log(age);
+
+age = age + 1;
+
+console.log(age);
+
+age++;
+
+console.log(age);
+
+age += 1;
+
+console.log(age);
+
+const firstName = "Sebastian";
+console.log(firstName + age);
+
+console.log(1 % 2);
+console.log(2 % 2);
+console.log(3 % 2);
+console.log(4 % 2);
+console.log(5 % 2);
+console.log(6 % 2);
+
+let isOn = true;
+isOn = !isOn;
+
+console.log(isOn);
