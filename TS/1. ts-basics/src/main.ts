742b9f2d6e87589bd95f71221080b2dcaf3c9a0a
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2025-11-03 11:40:17 +0100

    Restructure Added ts projects

---
+++ TS/1. ts-basics/src/main.ts
@@ -0,0 +1,5 @@
+let firstname: string = "Sebastian";
+// firstname = "46";
+
+console.log(firstname);
+const age = 46;
---
+++ TS/2. ts-vite/src/main.ts
@@ -0,0 +1,8 @@
+import "./style.scss";
+import { add } from "./utils";
+
+const firstname: string = "Sebastian";
+console.log(firstname);
+
+const s = add(10, 20);
+console.log(s);
