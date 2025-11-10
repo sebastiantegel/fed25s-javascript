5d9a8850b10553723bfad76d5e7a18db04b20e58
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2025-11-10 09:41:45 +0100

    Added promise in demo

--- TS/4. ts-fetch/src/main.ts
+++ TS/4. ts-fetch/src/main.ts
@@ -1,1 +1,22 @@
 import "./style.css";
+
+const x = 100;
+
+const p = new Promise((resolve: (value: boolean) => void, reject) => {
+  setTimeout(() => {
+    if (x >= 10) {
+      resolve(true);
+    } else {
+      reject();
+    }
+  }, 4000);
+});
+
+p.then(
+  (ok) => {
+    console.log("Det fungerar:", ok);
+  },
+  () => {
+    console.error("Det blev ett fel");
+  }
+);
