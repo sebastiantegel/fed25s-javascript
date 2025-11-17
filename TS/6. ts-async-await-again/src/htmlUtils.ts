446a372aba3d34611c0e3c8f0f34364feadf908e
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2025-11-17 11:10:16 +0100

    Implemented bootstrap modal for details

--- TS/6. ts-async-await-again/src/htmlUtils.ts
+++ TS/6. ts-async-await-again/src/htmlUtils.ts
@@ -1,4 +1,5 @@
 import type { Movie } from "./models/Movie";
+import type { MovieExt } from "./models/MovieExt";
 import { getMovieById } from "./services/movieService";
 
 export const createHtml = (movies: Movie[]) => {
@@ -20,14 +21,48 @@ export const createHtml = (movies: Movie[]) => {
     imgContainer.className = "imgContainer";
 
     movieContainer.addEventListener("click", async () => {
+      const body = document.getElementById("modalBody");
+      if (body) {
+        body.innerHTML = "";
+      }
       const movieDetails = await getMovieById(movie.imdbID);
-      console.log(movieDetails);
+      createHtmlForMovieDetails(movieDetails);
     });
     movieContainer.className = "movie";
+    movieContainer.setAttribute("data-bs-toggle", "modal");
+    movieContainer.setAttribute("data-bs-target", "#staticBackdrop");
 
     imgContainer.appendChild(img);
     movieContainer.appendChild(title);
     movieContainer.appendChild(imgContainer);
     moviesSection?.appendChild(movieContainer);
   });
+};
+
+export const createHtmlForMovieDetails = (movie: MovieExt) => {
+  const title = document.getElementById("staticBackdropLabel");
+  if (title) {
+    title.innerHTML = movie.Title;
+  }
+
+  const body = document.getElementById("modalBody");
+  if (body) {
+    const imgContainer = document.createElement("div");
+    const img = document.createElement("img");
+    const actors = document.createElement("p");
+    const director = document.createElement("p");
+    const plot = document.createElement("p");
+
+    img.src = movie.Poster;
+    img.alt = movie.Title;
+    actors.innerHTML = movie.Actors;
+    director.innerHTML = movie.Director;
+    plot.innerHTML = movie.Plot;
+
+    imgContainer.appendChild(img);
+    body.appendChild(imgContainer);
+    body.appendChild(plot);
+    body.appendChild(actors);
+    body.appendChild(director);
+  }
 };
