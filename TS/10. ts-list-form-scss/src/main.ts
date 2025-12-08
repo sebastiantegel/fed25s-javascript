bd2b845247299f411d4d6b5280faad1f3b011867
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2025-12-08 10:35:42 +0100

    Added project

---
+++ TS/10. ts-list-form-scss/src/main.ts
@@ -0,0 +1,60 @@
+import type { Movie } from "./models/Movie";
+import type { OmdbResponse } from "./models/OmdbResponse";
+import "./style.css";
+
+const numbers: number[] = [1, 5, 3, 1, 2, 8];
+const ascending = true;
+
+let movies: Movie[] = [];
+
+const response = await fetch("https://omdbapi.com/?apikey=416ed51a&s=star");
+const data: OmdbResponse = await response.json();
+movies = data.Search;
+
+// sort
+numbers.sort((a, b) => {
+  if (ascending) {
+    if (a > b) return 1;
+    if (a < b) return -1;
+    return 0;
+  } else {
+    if (a < b) return 1;
+    if (a > b) return -1;
+    return 0;
+  }
+});
+
+console.log(numbers);
+
+console.log([...movies]);
+
+movies.sort((a, b) => {
+  if (a.Title.toLowerCase() > b.Title.toLowerCase()) return 1;
+  if (a.Title.toLowerCase() < b.Title.toLowerCase()) return -1;
+  return 0;
+});
+
+console.log(movies);
+
+// filtrera
+const filteredNumbers = numbers.filter((n) => n > 2);
+console.log(filteredNumbers);
+
+const nonStarWarsMovies = movies.filter(
+  (movie) => !movie.Title.startsWith("Star Wars")
+);
+console.log(nonStarWarsMovies);
+
+// hitta
+const foundNumber = numbers.find((n) => n === 5);
+console.log(foundNumber);
+
+const foundMovie = movies.find((movie) => movie.Title.startsWith("Star Wars"));
+console.log(foundMovie);
+
+// omvandla
+const doubles = numbers.map((n) => {
+  return n * 2;
+});
+
+console.log(doubles);
